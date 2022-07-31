9029bc4696d27f1dbbaff1c9ada432f9c85209a9
Min-Su Kim <min-sukim@Min-Suui-MacBookPro-2.local>
2022-08-01 02:06:35 +0900

    🎉 프로젝트 생성

---
+++ Chews/Source/Application/ChewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChewsApp.swift
+//  Chews
+//
+//  Created by Min-Su Kim on 2022/08/01.
+//
+
+import SwiftUI
+
+@main
+struct ChewsApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
